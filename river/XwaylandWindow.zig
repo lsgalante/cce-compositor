riverwm/river

commit 35e800a31305c602e07e5ec8169030edc4cabb6d

XwaylandWindow: send correct position during fullscreen
The position requested by the window manager does not affect the
actual position while a window is fullscreen.

Reported-by: Ben Buhse <me@benbuhse.email>

river/XwaylandWindow.zig
@@ -104,8 +104,8 @@ pub fn configure(xwindow: *XwaylandWindow) bool {
     // until after a rendering sequence is completed. Therefore, configure()
     // is called both on manageFinish() and renderFinish() for Xwayland windows.
     // Frame perfection is not achievable for Xwayland windows in any case.
-    if (window.rendering_requested.x != xwindow.xsurface.x or
-        window.rendering_requested.y != xwindow.xsurface.y or
+    if (window.box.x != xwindow.xsurface.x or
+        window.box.y != xwindow.xsurface.y or
         width != xwindow.xsurface.width or
         height != xwindow.xsurface.height)
     {

lsgalante/cce-compositor

commit 374a958ee2ef2ec9702f408b373d0bc163f76691

XwaylandWindow: send correct position take 2
Fixes: 35e800a31305

river/XwaylandWindow.zig
@@ -110,8 +110,8 @@ pub fn configure(xwindow: *XwaylandWindow) bool {
         height != xwindow.xsurface.height)
     {
         xwindow.xsurface.configure(
-            math.lossyCast(i16, window.rendering_requested.x),
-            math.lossyCast(i16, window.rendering_requested.y),
+            math.lossyCast(i16, window.box.x),
+            math.lossyCast(i16, window.box.y),
             math.lossyCast(u16, width),
             math.lossyCast(u16, height),
         );